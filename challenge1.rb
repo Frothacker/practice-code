9ffcc323066f000abebc1c52bbcc1cd29a91c487
Finn <finn.rothacker@gmail.com>
2018-03-14 16:32:29 +0800

    ruby challenge

--- challenge1.rb
+++ challenge1.rb
@@ -5,16 +5,17 @@ find the sum of all the numbers between including them too and return it.
 If the two numbers are equal return a or b.
 
 a and b are not ordered
-=end
+=end 
 
-
+# for adding user inputs
 puts "input a"
 x = gets.chomp.to_i
 puts "input b"
 y = gets.chomp.to_i
 
-
 def get_sum(a,b)
+  low  = 0
+  high = 0
   
   # sorting values
   if a == b
@@ -27,19 +28,34 @@ def get_sum(a,b)
     high = a
   end
 
+  # to ensure high value has been added to the sum
+  sum    = high
+
   # counter to add all values between low and high to sum 
-  sum = 0
   until low == high
     sum = sum + low
     low += 1
   end
+
   return sum
-  
 end
 
-sum_x_y = get_sum(x,y)
+=begin
 
-puts sum_x_y
+# tests + explained
+puts get_sum(1, 0)    # 1 + 0 = 1
+puts get_sum(1, 2)    # 1 + 2 = 3
+puts get_sum(0, 1)    # 0 + 1 = 1
+puts get_sum(1, 1)    # 1 Since both are same
+puts get_sum(-1, 0)   # -1 + 0 = -1
+puts get_sum(-1, 2)   # -1 + 0 + 1 + 2 = 2
+
+
+
+# output: 1 2 1 1 -1 2
+puts "#{get_sum(1, 0)} #{get_sum(1, 2)} #{get_sum(0, 1)} #{get_sum(1, 1)} #{get_sum(-1, 0)} #{get_sum(-1, 2)}"
+
+=end
 
 
 
