5b8b3fd6408faf4827ba9ccb5b251a2ea4c3e338
Finn <finn.rothacker@gmail.com>
2018-03-16 16:03:39 +0800

    refactored code wars challenge2

--- challenge2.rb
+++ challenge2.rb
@@ -1,10 +1,9 @@
 # Return the sum of all the multiples of 3 and 5 below the number entered.
 # Note: If the number is a multiple of both 3 and 5, only count it once.
-
 def solution(number)
-  number -=1 and sum = 0
-  for x in number.downto(0) do
-    x%5 == 0 ? (sum = sum+x) :  if(x%3 == 0) then sum = sum+x 
-    end 
-  end    
+  number -=1 and sum = 0 and for x in number.downto(0) do x%5 == 0 ? (sum = sum+x) : if(x%3 == 0) then sum = sum+x   
+  end end and return sum 
 end 
+
+
+
