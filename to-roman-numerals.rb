51da363c5fe1a31d72cc757d3582f376524bb71c
Finn <finn.rothacker@gmail.com>
2018-04-02 12:48:33 +0800

    functioning number to riman numerals converter

--- to-roman-numerals.rb
+++ to-roman-numerals.rb
@@ -6,17 +6,54 @@
 
 # to_roman takes a number as input and returns that number using Roman numerals
 # See http://en.wikipedia.org/wiki/Roman_numerals
+loop do
 
-# It can be tricky to handle the cases where "IV" means 4.  As a first pass,
-# don't hesitate to implement a simpler version where 4 is denoted by "IIII".
-#
-# Remember: the priority is FEEDBACK, even if your implementation can't handle
-# every single case.
+  def to_roman(num)
+    roman = ""
+    num.times { roman.prepend("I") }
+     
+  #initial solution
+    # roman.gsub!( "I" * 1000, "M")
+    # roman.gsub!( "I" * 500, "D")
+    # roman.gsub!( "I" * 100, "C")
+    # roman.gsub!( "I" * 50, "L")
+    # roman.gsub!( "I" * 10, "X")
+    # roman.gsub!( "I" * 9, "IV")
+    # roman.gsub!( "I" * 8, "VIII")
+    # roman.gsub!( "I" * 7, "VII")
+    # roman.gsub!( "I" * 6, "VI")
+    # roman.gsub!( "I" * 5, "V")
+    # roman.gsub!( "I" * 4, "IV")
+    
+  # revised solution
+    replacement = { 
+      "I" * 1000 => "M",
+      "I" * 500 => "D",
+      "I" * 100 => "C",
+      "I" * 50 => "L",
+      "I" * 10 => "X",
+      "I" * 9 => "IV",
+      "I" * 8 => "VIII",
+      "I" * 7 => "VII",
+      "I" * 6 => "VI",
+      "I" * 5 => "V",
+      "I" * 4 => "IV" }
+    replacement.each do |i , numeral| 
+      roman.gsub!(i , numeral) 
+    end
 
-def to_roman(num)
-end
+    puts roman
+  end
 
-if __FILE__ == $PROGRAM_NAME
-  return
-  # What are some test cases?
-end
+  puts "what number"
+  number = gets.chomp.to_i
+  to_roman(number)
+
+  if __FILE__ == $PROGRAM_NAME
+    # What are some test cases?
+  end
+
+end #loop this is infinite loop. used for trying many roman numerals. 
+    #use 'control' + 'c' to stop it. 
+
+
