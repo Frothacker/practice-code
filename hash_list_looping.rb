b7a434933939944982446649b1dfb749e6b71790
Finn <finn.rothacker@gmail.com>
2018-04-06 14:44:29 +0800

    bug fixes

--- hash_list_looping.rb
+++ hash_list_looping.rb
@@ -2,22 +2,26 @@
 
 class Hashlist 
   def initialize
-    @nodes = {}
+    @nodes = {0 => { :id => "HEAD", :next => 1, :prev => 0 }}
     @pointer = 0
   end
 
 
   def add_node(x)
-    @nodes.merge!({0 => { :id => "first", :next => 1, :prev => 0}})
+    # delete TAIL node
+    @nodes = @nodes.delete_if do |key,vhash|
+      vhash[:id] == "TAIL"
+    end
+    # create x nodes
     x.times do 
       @nodes.merge!({ @nodes.length => {:id => @nodes.length, :next => (@nodes.length + 1), :prev => (@nodes.length - 1)} }) 
     end 
-
+    # make HEAD node point to TAIL node id 
     @nodes[0][:prev] = @nodes.length
-
-    @nodes.merge!({@nodes.length => { :id => "last", :next => 0, :prev => (@nodes.length-1)}})
-    puts @nodes
+    # add Tail node
+    @nodes.merge!({@nodes.length => { :id => "TAIL", :next => 0, :prev => (@nodes.length-1)}})
   end
+
 
   def next 
     @pointer = @nodes[@pointer][:next]
@@ -35,14 +39,13 @@ end # Hashlist end
 
 
 =begin
-
  used for irb 
 
 
 irb
 require "./hash_list_looping"
 l = Hashlist.new
-l.add_node(5)
+l.add_node(4)
 l.next
 
 
